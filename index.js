ee1540ae6f1899a89c3b91f84fbd9c69473d4aff
vishalicious213 <vishalicious213@gmail.com>
2023-05-31 11:52:43 -0700

    getting a single movie's info from omdb api request

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ searchBtn.addEventListener("click", submitSearch)
 
 function submitSearch() {
     console.log(searchField.value)    
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=8c98ceb6&t=${searchField.value}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
 }
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=8c98ceb6
