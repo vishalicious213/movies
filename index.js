1e00bb461eaf6b119cdace6550757b0acfc64efc
vishalicious213 <vishalicious213@gmail.com>
2023-06-01 13:56:15 -0700

    cleaned up code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const main = document.getElementById("main")
 
 let filmsArray = []
 let currentFilm = {}
-let filmsHtml = "test"
 
 // ⬇️ EVENT LISTENERS ⬇️
 
@@ -17,36 +16,17 @@ searchBtn.addEventListener("click", function() {
 async function getFilms() {
     const response = await fetch(`http://www.omdbapi.com/?apikey=8c98ceb6&s=${searchField.value}`)
     const data = await response.json()
-    // console.log(data)
-    // console.log(typeof data.Search)
-    // console.log(data.Search[0])
     const responseArray = data.Search
-    // console.log(responseArray[1])
 
-    // console.log("responseArray", typeof responseArray)
-
-    // responseArray.forEach(item => getFilmDetails(item.imdbID))
     await Promise.all(responseArray.map(item => getFilmDetails(item.imdbID)))
-    // console.log(responseArray)
-    // console.log(filmsArray)
-    renderfilmsArray()
-    console.log(filmsHtml)
+    renderFilmsArray()
 }
 
 async function getFilmDetails(id) {
     const response = await fetch(`http://www.omdbapi.com/?apikey=8c98ceb6&i=${id}`)
     const data = await response.json()
 
-    // console.log("film details", typeof data)
-    // console.log("film data details", data)
-    // console.log(data.Title)
     filmsArray.push(data)
-    buildHTML(data)
-
-    // console.log("filmsArray type", typeof filmsArray)
-    // console.log(filmsArray)
-
-    // console.log(filmsArray)
 }
 
 function buildHTML(data) {
@@ -65,29 +45,9 @@ function renderEmptyFilms() {
     `
 }
 
-function renderfilmsArray() {
+function renderFilmsArray() {
     console.log(filmsArray)
-    console.log(filmsArray[0])
-    console.log(filmsArray[9])
-    console.log(filmsHtml)
     main.innerHTML = ""
-    // let currentFilm = getFilm(filmsArray[0].imdbID)
-    // let currentFilm = filmsArray[0]
-
-    // getFilm(filmsArray[0].imdbID)
-    // console.log(currentFilm)
-    console.log("TEST1")
-
-    console.log(typeof filmsArray)
-
-    // let copy = filmsArray
-    // console.log(JSON.stringify(copy[0]))
-    
-    // console.log(typeof filmsArray[0])
-    // console.log(Object.values(filmsArray))
-    // console.log(filmsArray)
-
-    // filmsArray.forEach(item => console.log("test"))
 
     // filmsArray.forEach(item => {
     //     console.log(item)
@@ -102,9 +62,6 @@ function renderfilmsArray() {
     //     //     </div>
     //     // `
     // })
-
-    console.log("TEST2")
-
 }
 
 renderEmptyFilms()
